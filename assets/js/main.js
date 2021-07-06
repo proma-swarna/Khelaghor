2a0dc2df99f064ccbaa4690b703f08294bb80d94
unknown <promaswarna@gmail.com>
2021-07-07 00:31:50 +0600

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,16 @@ $(".clients").owlCarousel({
     margin: 30,
     loop: 'false',
     autoplay: 'false',
-    autoplayTimeout: 3000,
+    autoplayTimeout: 5000,
     center: 'true',
     items: 3
 });
+// init Isotope
+    var $grid = $('.table-details').isotope({
+    // options
+    });
+// filter items on button click
+    $('.list-menu').on( 'click', 'li', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+    });
